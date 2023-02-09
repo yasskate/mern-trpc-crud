6973a7c839b72817a9a7f8c553f761f6520bbcdd
Yasser Batas - yasskate <yassk8@gmail.com>
2023-02-09 01:51:47 -0600

    Implement cors on trpc server

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan'
 import  * as trpcExpress from '@trpc/server/adapters/express'
 import { router, createContext } from './trpc'
 import { notesRouter } from './routes/notes'
+import cors from 'cors'
 
 const app = express()
 
@@ -10,11 +11,15 @@ const appRouter = router({
   note: notesRouter
 })
 
+app.use(cors())
 app.use(morgan('dev'))
 
 app.use('/trpc', trpcExpress.createExpressMiddleware({
   router: appRouter,
   createContext
 }))
+
+export type AppRouter = typeof appRouter
+
 export default app
 
