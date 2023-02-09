f14209c746ab17513a7c1fb1ac9fa672e459aa90
Yasser Batas <yassk8@gmail.com>
2023-02-09 01:53:34 -0600

    Implements trpc client connection (#1)
    * Implement cors on trpc server
    
    * Implements trpc client setup

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,7 +1,11 @@
 import { publicProcedure, router } from '../trpc'
 
 const getNotes = publicProcedure.query(() => {
-  return []
+  return [{
+    id: 1,
+    title: 'Note 1',
+    content: 'Content 1'
+  }]
 })
 
 export const notesRouter = router({
