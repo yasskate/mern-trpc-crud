a75126683783999bba12512eff8ac3a659d6b22e
Yasser Batas - yasskate <yassk8@gmail.com>
2023-02-09 19:46:33 -0600

    Add create note trcp method

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,4 +1,5 @@
 import { publicProcedure, router } from '../trpc'
+import { z } from 'zod'
 
 const getNotes = publicProcedure.query(() => {
   return [{
@@ -8,6 +9,15 @@ const getNotes = publicProcedure.query(() => {
   }]
 })
 
+const createNote = publicProcedure.input(z.object({
+  title: z.string(),
+  description: z.string()
+})).mutation(({ input }) => {
+  console.log("Input", input)
+  return "recieved"
+})
+
 export const notesRouter = router({
-  get: getNotes
+  get: getNotes,
+  create: createNote
 })
